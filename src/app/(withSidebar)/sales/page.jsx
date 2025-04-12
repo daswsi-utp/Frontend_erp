965638163be5d725eebc62922de1e66410b5a83e
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-04-12 14:01:28 -0500

    pages

---
+++ src/app/(withSidebar)/sales/page.jsx
@@ -0,0 +1,8 @@
+
+export default function RRHH() {
+  return (
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          This is one module from human resources 
+        </div>
+  );
+}
