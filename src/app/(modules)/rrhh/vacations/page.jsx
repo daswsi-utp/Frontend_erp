82c31a3d24e67c60afa5747601ea8636e81a9899
Yamada-decs <u22100700@utp.edu.pe>
2025-04-20 23:14:13 -0500

    Vacations page created

--- src/app/(modules)/rrhh/vacations/page.jsx
+++ src/app/(modules)/rrhh/vacations/page.jsx
@@ -1,8 +1,57 @@
+import VacationsTable from "@/modules/rrhh/vacations/tables/VacationsTable";
+
+const vacations = [
+  {
+    id: 1,
+    employee: {
+      id: 2,
+      firstName: "Daniel",
+      lastName: "Cabrera"
+    },
+    startDate: '2025-04-21',
+    endDate: '2026-04-21',
+    daysTaken: '70',
+    status: 'APROVADO',
+    requestedAt: '2004-10-20'
+  },
+  {
+    id: 2,
+    employee: {
+      id: 2,
+      firstName: "Daniel",
+      lastName: "Cabrera"
+    },
+    startDate: '2026-04-21',
+    endDate: '2027-04-21',
+    daysTaken: '70',
+    status: 'PENDIENTE',
+    requestedAt: '2025-04-20'
+  },
+  {
+    id: 3,
+    employee: {
+      id: 2,
+      firstName: "Daniel",
+      lastName: "Cabrera"
+    },
+    startDate: '2027-04-21',
+    endDate: '2028-04-21',
+    daysTaken: '70',
+    status: 'DESAPROVADO',
+    requestedAt: '2004-10-20'
+  }
+];
+
 const Vacations = () => {
     return (
-      <div>
-        <h1>Pagina de Vacaciones y asi</h1>
-      </div>
+      <>
+        <div className="w-full flex justify-between items-center mb-2">
+          <h1 className="text-2xl font-bold tracking-tight text-gray-800 dark:text-white">Vacaciones Registradas</h1>
+        </div>
+        <VacationsTable
+          vacations={vacations}
+        />
+      </>
     );
 };
   
