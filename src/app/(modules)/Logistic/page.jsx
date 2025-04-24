a2ed7fe96ce7645aa37494a9c864b6d7fc692700
SebastianT28 <sebasticlavilca@gmail.com>
2025-04-24 10:15:02 -0500

    Implementacion de vista principal page.jsx de Logistica

--- src/app/(modules)/Logistic/page.jsx
+++ src/app/(modules)/Logistic/page.jsx
@@ -1,17 +1,10 @@
 'use client';
+import DashboardTabs from "@/modules/logistic/dashboard/dashboardTabs";
 
-import InventoryDashboard from "./Dashboard/dashboard";
-
-export default function Page() {
+export default function FullDashboardPage() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-950">
-      <header className="border-b border-gray-200 dark:border-gray-800 px-6 py-4">
-        <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Dashboard de Inventario</h1>
-      </header>
-
-      <main className="p-6">
-        <InventoryDashboard />
-      </main>
+    <div className="p-6">
+      <DashboardTabs />
     </div>
   );
 }
