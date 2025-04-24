3358c3202b944c663da6fa48a4eca3668388f7bc
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-04-24 11:01:44 -0500

    sspinner corrected

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -1,9 +1,12 @@
+// src/components/shared/Spinner.js
 import React from 'react';
 
 const Spinner = ({ color = 'text-primary', size = 'h-8 w-8' }) => {
   return (
-   <div className="spinner">Cargando...</div>
-  )
+    <div
+      className={`border-4 border-t-transparent border-gray-200 rounded-full ${size} ${color} animate-spin`}
+    ></div>
+  );
 };
 
 export default Spinner;
