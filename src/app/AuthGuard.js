6b6903f8bc010e43b2ef5904ead1154c106f5507
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2025-06-02 00:55:51 -0500

    page unathorized

---
+++ src/app/AuthGuard.js
@@ -0,0 +1,46 @@
+'use client'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Unauthorized from '@/features/auth/components/Unauthorized'// tu componente
+
+const protectedRoutes = [
+  '/crm',
+  '/customers',
+  '/logistic',
+  '/manufacture',
+  '/planning',
+  '/rrhh',
+  '/sales'
+]
+
+export default function AuthGuard({ children }) {
+  const pathname = usePathname()
+  const router = useRouter()
+  const [authorized, setAuthorized] = useState(null) // null: no decidido, true: ok, false: no autorizado
+
+  useEffect(() => {
+    const token = localStorage.getItem('auth-token')
+    const user = localStorage.getItem('current_user')
+
+    if (protectedRoutes.some(route => pathname.startsWith(route))) {
+      if (!token || !user) {
+        setAuthorized(false)
+        return
+      }
+    }
+
+    setAuthorized(true)
+  }, [pathname])
+
+  if (authorized === null) {
+    // Mientras verifica, puedes mostrar spinner o nada
+    return null
+  }
+
+  if (authorized === false) {
+    // Mostrar pantalla de acceso denegado
+    return <Unauthorized />
+  }
+
+  return children
+}
